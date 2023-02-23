810a29a60a940d003da1d407bcb69eecae636e80
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-02-23 16:58:00 -0300

    states and selectors asynchronous

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,40 @@
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { useState } from 'react';
+import { useRecoilState, useRecoilValueLoadable } from 'recoil'
+
+// recoil: atoms
 import { atomPokemon } from '../../store/atoms';
-import { selectorPokemonLength } from '../../store/selectors';
+
+// recoil: selectors
+import { selectorGetPokemon } from '../../store/selectors';
 
 const Home = () => {
+  // local: states
+  const [searchPokemon, setSearchPokemon] = useState('');
+
+  // recoil: states
   const [pokemon, setPokemon] = useRecoilState(atomPokemon);
-  const pokemonLength = useRecoilValue(selectorPokemonLength);
+
+  // recoil: loadable
+  const getLoadablePokemon = useRecoilValueLoadable(selectorGetPokemon);
+
+  console.log(getLoadablePokemon?.contents)
 
   return (
     <div>
-      <input type="text" onChange={(event) => setPokemon(event.target.value)} />
-      {pokemon}
-      <p>Length: {pokemonLength}</p>
+      <input type="text" onChange={(event) => setSearchPokemon(event.target.value)} />
+      <button onClick={() => setPokemon(searchPokemon)}>Procurar</button>
+      {getLoadablePokemon?.state === "loading" && <div>Loading ...</div>}
+      {getLoadablePokemon?.state === "hasValue" &&
+        getLoadablePokemon?.contents !== undefined && (
+          <div>
+            <img
+              src={getLoadablePokemon?.contents?.sprites?.front_default}
+              alt={`pokemon-${getLoadablePokemon?.contents?.name}`}
+            />
+            <h3>{getLoadablePokemon?.contents?.name}</h3>
+          </div>
+        )
+      }
     </div>
   )
 };
--- src/store/selectors/index.ts
+++ src/store/selectors/index.ts
@@ -1,10 +1,22 @@
 import { selector } from 'recoil'
+
+// api
+import { requester } from '../../api/requester';
+
+// recoil: atoms
 import { atomPokemon } from '../atoms'
 
-export const selectorPokemonLength = selector({
-  key: 'selectorPokemonLength',
-  get: ({ get }) => {
+export const selectorGetPokemon = selector({
+  key: 'selectorGetPokemon',
+  get: async ({ get }) => {
     const pokemon = get(atomPokemon);
-    return pokemon?.length;
+
+    if(pokemon) {
+      const { data } = await requester({
+        baseURL: "https://pokeapi.co/api/v2",
+      }).get(`/pokemon/${pokemon.toLowerCase().trim()}`);
+
+      return data;
+    }
   }
 })
